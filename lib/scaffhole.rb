fc19a883a8a32fe342c6e20785279b2137ebf76a
Pat Nakajima <patnakajima@gmail.com>
2008-10-01 03:22:27 -0400

    removed unnecessary klass.rb

--- lib/scaffhole.rb
+++ lib/scaffhole.rb
@@ -1,7 +1,15 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), 'scaffhole')
 
 require 'resource'
-require 'index'
-require 'klass'
 
-ActionController::Base.send :include, Scaffhole::Klass
+ActionController::Base.class_eval do
+  def self.scaffhole(name)
+    klass_name = "#{name.to_s.pluralize}_controller".classify
+    klass = klass_name.constantize rescue begin
+      Object.const_set(klass_name, Class.new(self))
+    end
+    
+    klass.send(:include, Scaffhole::Resource)
+    klass.klass = name.to_s.classify.constantize
+  end
+end
--- lib/scaffhole/resource.rb
+++ lib/scaffhole/resource.rb
@@ -1,7 +1,15 @@
+require 'index'
+
 module Scaffhole
   module Resource
     def self.included(base)
       base.cattr_accessor :klass
+      base.send(:include, Scaffhole::Index)
+      base.send(:append_view_path, File.join(File.dirname(__FILE__), '..', '..', 'views'))
+    end
+    
+    def klass
+      self.class.klass
     end
   end
 end
